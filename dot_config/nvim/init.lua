40f3e962bfc1ebfbd5cb900feb82382300f382b9
Ben Godfrey <ben@ben2.com>
2024-08-20 17:19:48 +0100

    Remove old lsp-zero/lspconfig/nvim-cmp configuration

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -277,109 +277,6 @@ require("lazy").setup({
         })
       end
     },
-
-    -- lsp-zero
-    -- {
-    --   'VonHeikemen/lsp-zero.nvim',
-    --   branch = 'v4.x',
-    --   lazy = true,
-    --   config = false,
-    -- },
-
-    -- mason to install language servers
-    -- {
-    --   'williamboman/mason.nvim',
-    --   lazy = false,
-    --   config = true,
-    -- },
-
-    -- LSPconfig
-    --{
-    --  'neovim/nvim-lspconfig',
-    --  cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-    --  event = {'BufReadPre', 'BufNewFile'},
-    --  dependencies = {
-    --    -- Mason to install language servers
-    --    { 'williamboman/mason-lspconfig.nvim' },
-
-    --    -- Autocompletion
-----        {
-----          'hrsh7th/nvim-cmp',
-----          event = 'InsertEnter',
-----          dependencies = {
-----  	    { 'hrsh7th/cmp-buffer' },
-----	    { 'hrsh7th/cmp-nvim-lsp' },
-----	    { 'hrsh7th/cmp-nvim-lua' },
-----	    { 'hrsh7th/cmp-path' },
-----	    { 'saadparwaiz1/cmp_luasnip' },
-----            { 'L3MON4D3/LuaSnip', version = "v2.*" },
-----            { 'rafamadriz/friendly-snippets' }
-----          },
-----          config = function()
-----            local cmp = require('cmp')
-----
-----            require('luasnip.loaders.from_vscode').lazy_load()
-----
-----            cmp.setup({
-----              sources = {
-----                { name = 'nvim_lsp' },
-----                { name = 'nvim_lua' },
-----                { name = 'luasnip' },
-----                { name = 'path' },
-----                { name = 'buffer' },
-----              },
-----              mapping = cmp.mapping.preset.insert({
-----                ['<C-Space>'] = cmp.mapping.complete(),
-----                ['<C-u>'] = cmp.mapping.scroll_docs(-4),
-----                ['<C-d>'] = cmp.mapping.scroll_docs(4),
-----              }),
-----              snippet = {
-----                expand = function(args)
-----                  vim.snippet.expand(args.body)
-----                end,
-----              },
-----            })
-----          end
-----        },
-    --  },
-    --  config = function()
-    --    local lsp_zero = require('lsp-zero')
-
-    --    -- lsp_attach is where you enable features that only work
-    --    -- if there is a language server active in the file
-    --    local lsp_attach = function(client, bufnr)
-    --      local opts = { buffer = bufnr }
-
-    --      vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-    --      vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-    --      vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-    --      vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-    --      vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-    --      vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-    --      vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-    --      vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-    --      vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-    --      vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-    --    end
-
-    --    lsp_zero.extend_lspconfig({
-    --      sign_text = true,
-    --      lsp_attach = lsp_attach,
-    --      capabilities = require('cmp_nvim_lsp').default_capabilities()
-    --    })
-
-    --    require('mason-lspconfig').setup({
-    --      ensure_installed = {},
-    --      handlers = {
-    --        -- this first function is the "default handler"
-    --        -- it applies to every language server without a "custom handler"
-    --        function(server_name)
-    --          require('lspconfig')[server_name].setup({})
-    --        end,
-    --      }
-    --    })
-    --  end
-    --}
   },
 
   -- Configure any other settings here. See the documentation for more details.
@@ -394,12 +291,10 @@ require("lazy").setup({
   },
 })
 
-local lsp_zero = require("lsp-zero")
-
 -- lsp_attach is where you enable features that only work
 -- if there is a language server active in the file
 local lsp_attach = function(client, bufnr)
-  local opts = {buffer = bufnr}
+  local opts = { buffer = bufnr }
 
   vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
   vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
@@ -413,12 +308,16 @@ local lsp_attach = function(client, bufnr)
   vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
 end
 
+-- configure lspzero
+local lsp_zero = require("lsp-zero")
+
 lsp_zero.extend_lspconfig({
   sign_text = true,
   lsp_attach = lsp_attach,
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
+-- configure mason
 require("mason").setup({})
 require("mason-lspconfig").setup({
   handlers = {
