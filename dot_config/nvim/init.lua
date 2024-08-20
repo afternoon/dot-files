94888e527567ed0ce797efba5bb48a52dabe1a84
Ben Godfrey <ben@ben2.com>
2024-08-20 17:35:10 +0100

    Rollback some tweaks to neovim completion/snippets config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -221,8 +221,8 @@ require("lazy").setup({
     { "ntpeters/vim-better-whitespace" },
 
     -- lsp-zero v4
-    { "VonHeikemen/lsp-zero.nvim", branch = "v4.x", lazy = false },
-    { "neovim/nvim-lspconfig", lazy = false },
+    { "VonHeikemen/lsp-zero.nvim", branch = "v4.x" },
+    { "neovim/nvim-lspconfig" },
 
     -- language server management
     { 'williamboman/mason.nvim' },
@@ -233,8 +233,8 @@ require("lazy").setup({
       "hrsh7th/nvim-cmp",
       dependencies = {
         { 'L3MON4D3/LuaSnip', version = "v2.*" },
-        { "saadparwaiz1/cmp_luasnip" },
         { 'rafamadriz/friendly-snippets' },
+        { "saadparwaiz1/cmp_luasnip", },
         { "hrsh7th/cmp-buffer" },
         { "hrsh7th/cmp-nvim-lsp" },
         { "hrsh7th/cmp-nvim-lua" },
@@ -291,29 +291,13 @@ require("lazy").setup({
   },
 })
 
--- lsp_attach is where you enable features that only work
--- if there is a language server active in the file
-local lsp_attach = function(client, bufnr)
-  local opts = { buffer = bufnr }
-
-  vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
-  vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
-  vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
-  vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
-  vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
-  vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
-  vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
-  vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
-  vim.keymap.set({"n", "x"}, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
-  vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
-end
-
 -- configure lspzero
 local lsp_zero = require("lsp-zero")
-
 lsp_zero.extend_lspconfig({
   sign_text = true,
-  lsp_attach = lsp_attach,
+  lsp_attach = function(client, bufnr)
+    lsp_zero.default_keymaps({ buffer = bufnr })
+  end,
   capabilities = require("cmp_nvim_lsp").default_capabilities(),
 })
 
